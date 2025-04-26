d2bebbde2ee2c12cde14bdc48e424ae2921a682d
Gian Dolor <gdolor@gbox.adnu.edu.ph>
2025-04-26 15:40:53 +0800

    Reset everything from scractch. Added PIN initialization for both Client and Server.

--- guessThePinClient.c
+++ guessThePinClient.c
@@ -1,3 +1,17 @@
+/*#----------------------------------------------------------------------------------------------------------------#
+# Filename : guessThePinclient.c#
+# Author : Dolor, Llagas, Mien, Raposa, Valencia#
+# Last Modified : April 3, 2025 #
+# Description : A proposed Guess The Pin project #
+# Honor Code : This is my own program. I have not received any #
+# unauthorized help in completing this work. I have not #
+# copied from my classmate, friend, nor any unauthorized #
+# resource. I am well aware of the policies stipulated #
+# in the handbook regarding academic dishonesty. #
+# If proven guilty, I won't be credited any points for #
+# this exercise. #
+#-------------------------------------------------------------------------------------------------------------#*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,79 +21,29 @@
 #include <netdb.h>
 #include <unistd.h>
 
-// Error handler
 void die_with_error(char *error_msg){
     printf("%s", error_msg);
     exit(-1);
 }
 
-// Send a clue message
-void sendclue(char *clue_msg,char *buffer, int client_sock ){
-    strcpy(buffer, clue_msg);
-    int n = send(client_sock, buffer, strlen(buffer), 0);
-}
-
-// Check the guess and send hints to server
-void check(char pin[3], char buffer[3], int client_sock){
-    int rdigit=0, rplace=0;
-
-    for (int i = 0; i < 3; i++) {
-        for (int o = 0; o < 3; o++) {
-            if (pin[o] == buffer[i]){
-                rdigit++;
-                break;
-            }
-        }
-    }
-
-    for (int i = 0; i < 3; i++) {
-        if (pin[i] == buffer[i]){
-            rplace++;
-        }
-    }
-
-    bzero(buffer, 256);
-
-    if (rdigit == 0){
-        sendclue("Nothing is correct.\n", buffer, client_sock);
-    } else if (rdigit == 1){
-        sendclue("1 digit is correct.\n", buffer, client_sock);
-    } else {
-        sprintf(buffer, "%d digits are correct.\n", rdigit);
-        sendclue(buffer, buffer, client_sock);
-    }
-
-    bzero(buffer, 256);
-
-    if (rplace == 0){
-        sendclue("Nothing is in the right place.\n", buffer, client_sock);
-    } else if (rplace == 1){
-        sendclue("1 digit is in the right place.\n", buffer, client_sock);
-    } else {
-        sprintf(buffer, "%d digits are in the right place.\n", rplace);
-        sendclue(buffer, buffer, client_sock);
-    }
-}
-
 int main(int argc,  char *argv[]){
-    int client_sock, port_no, n;
+    
+    int server_sock, client_sock,  port_no,  n;
     struct sockaddr_in server_addr;
     struct hostent *server;
-
-    char buffer[256];
+    char buffer[256], clientPin[3], serverPin[3], clientGuess[3], serverGuess[3];
+    
     if (argc < 3) {
         printf("Usage: %s hostname port_no",  argv[0]);
         exit(1);
     }
 
     printf("Client starting ...\n");
-
-    // Create socket
+    // Create a socket using TCP
     client_sock = socket(AF_INET,  SOCK_STREAM,  0);
     if (client_sock < 0) 
         die_with_error("Error: socket() Failed.");
 
-    // Resolve host
     printf("Looking for host '%s'...\n", argv[1]);
     server = gethostbyname(argv[1]);
     if (server == NULL) {
@@ -87,11 +51,14 @@ int main(int argc,  char *argv[]){
     }
     printf("Host found!\n");
 
-    // Connect to server
+    // Establish a connection to server
     port_no = atoi(argv[2]);
     bzero((char *) &server_addr,  sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,  (char *)&server_addr.sin_addr.s_addr, server->h_length);
+    bcopy((char *)server->h_addr,  
+         (char *)&server_addr.sin_addr.s_addr, 
+         server->h_length);
+         
     server_addr.sin_port = htons(port_no);
 
     printf("Connecting to server at port %d...\n", port_no);
@@ -99,37 +66,53 @@ int main(int argc,  char *argv[]){
         die_with_error("Error: connect() Failed.");
 
     printf("Connection successful!\n");
-
-    // Ask user to input the PIN
-    char pin[3];
-    printf("Enter a 3 digit pin\n");
+    
+    // Get client pin
+    printf("Enter a 3 digit pin\n> ");
     for (int i = 0; i < 3; i++) {
-        scanf(" %c", &pin[i]); // Added space to consume whitespace
+        scanf(" %c", &clientPin[i]);
     }
+    printf("Client PIN set to: %c %c %c\n", clientPin[0], clientPin[1], clientPin[2]);
+  
+    // Send client pin to server
+    n = send(client_sock, clientPin, 3, 0);
+    if (n < 0) die_with_error("Error: send() Failed (sending pin).");
+    
+    // Receive server pin
+    n = recv(client_sock, serverPin, 3, 0);
+    if (n < 0) die_with_error("Error: recv() Failed (receiving pin).");
+    
+    printf("Server PIN received.\n");
 
-    // Communication loop
+
+
+    // Communicate
     while (1) {
-        // Receive message from server
+        // Receive the message from the server
         bzero(buffer, 256);
         n = recv(client_sock, buffer, 255, 0);
         if (n < 0) die_with_error("Error: recv() Failed.");
-
-        check(pin, buffer, client_sock);
-
+        
+        printf("[server] > %s", buffer);
+        
+        // End communication if server exited
         if (strncmp(buffer, "exit", 4) == 0) break;
-
-        // Send guess to server
+        
+        // Client sends a message
         printf("< ");
         bzero(buffer, 256);
         fgets(buffer, 255, stdin);
+        
         n = send(client_sock, buffer, strlen(buffer), 0);
         if (n < 0) die_with_error("Error: send() Failed.");
-
+        
+        
+        // End communication if client exited
         if (strncmp(buffer, "exit", 4) == 0) break;
     }
 
-    // Clean up
     printf("Closing connection ...\n");
     close(client_sock);
+    
     return 0;
 }
--- guessThePinServer.c
+++ guessThePinServer.c
@@ -1,3 +1,16 @@
+/*----------------------------------------------------------------------------------------------------------------#
+# Filename : guessThePinServer.c#
+# Author : Dolor, Llagas, Mien, Raposa, Valencia#
+# Last Modified : April 3, 2025 #
+# Description : A proposed Guess The Pin project #
+# Honor Code : This is my own program. I have not received any #
+# unauthorized help in completing this work. I have not #
+# copied from my classmate, friend, nor any unauthorized #
+# resource. I am well aware of the policies stipulated #
+# in the handbook regarding academic dishonesty. #
+# If proven guilty, I won't be credited any points for #
+# this exercise. #
+#-------------------------------------------------------------------------------------------------------------# */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,135 +19,103 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-// Error handler
 void die_with_error(char *error_msg){
     printf("%s", error_msg);
     exit(-1);
 }
 
-// Send a clue message to client
-void sendclue(char *clue_msg, char *buffer, int client_sock ){
-    strcpy(buffer, clue_msg);
-    int n = send(client_sock, buffer, strlen(buffer), 0);
-}
-
-// Check guess and send clues to client
-void check(char pin[3], char buffer[3], int client_sock){
-    int rdigit=0, rplace=0;
-
-    // Count correct digits
-    for (int i = 0; i < 3; i++) {
-        for (int o = 0; o < 3; o++) {
-            if (pin[o] == buffer[i]){
-                rdigit++;
-                break;
-            }
-        }
-    }
-
-    // Count correct digits in correct positions
-    for (int i = 0; i < 3; i++) {
-        if (pin[i] == buffer[i]){
-            rplace++;
-        }
-    }
-
-    bzero(buffer, 256);
-
-    // Send feedback for correct digits
-    if (rdigit == 0){
-        sendclue("Nothing is correct.\n", buffer, client_sock);
-    } else if (rdigit == 1){
-        sendclue("1 digit is correct.\n", buffer, client_sock);
-    } else {
-        sprintf(buffer, "%d digits are correct.\n", rdigit);
-        sendclue(buffer, buffer, client_sock);
-    }
-
-    bzero(buffer, 256);
-
-    // Send feedback for correct positions
-    if (rplace == 0){
-        sendclue("Nothing is in the right place.\n", buffer, client_sock);
-    } else if (rplace == 1){
-        sendclue("1 digit is in the right place.\n", buffer, client_sock);
-    } else {
-        sprintf(buffer, "%d digits are in the right place.\n", rplace);
-        sendclue(buffer, buffer, client_sock);
-    }
-}
-
 int main(int argc, char *argv[]){
     int server_sock, client_sock, port_no, client_size, n;
-    char buffer[256];
+    char buffer[256], serverPin[3], clientPin[3], serverGuess[3], clientGuess[3];
     struct sockaddr_in server_addr, client_addr;
-
+    
     if (argc < 2) {
         printf("Usage: %s port_no", argv[0]);
         exit(1);
     }
 
     printf("Server starting ...\n");
-    // Create socket
+    // Create a socket for incoming connections
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    
     if (server_sock < 0) 
-        die_with_error("Error: socket() Failed.");
-
-    // Bind to port
+       die_with_error("Error: socket() Failed.");
+       
+    // Bind socket to a port
     bzero((char *) &server_addr, sizeof(server_addr));
     port_no = atoi(argv[1]);
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port_no);
-
+    server_addr.sin_family = AF_INET; // Internet address 
+    server_addr.sin_addr.s_addr = INADDR_ANY; // Any incoming interface
+    server_addr.sin_port = htons(port_no); // Local port
+    
     if (bind(server_sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) 
-        die_with_error("Error: bind() Failed.");
-
-    // Listen for connection
+       die_with_error("Error: bind() Failed.");
+       
+    // Mark the socket so it will listen for incoming connections
     listen(server_sock, 5);
     printf("Server listening to port %d ...\n", port_no);
+    
     printf("Waiting for connection(s) ...\n");
 
-    // Accept connection
+    // Accept new connection
     client_size = sizeof(client_addr);
     client_sock = accept(server_sock, (struct sockaddr *) &client_addr, &client_size);
+    
     if (client_sock < 0) 
         die_with_error("Error: accept() Failed.");
 
-    printf("Client successfully connected ...\n");
-
-    // Ask user to input the PIN
-    char pin[3];
-    printf("Enter a 3 digit pin\n");
+    printf("Client succesfully connected ...\n"); 
+    
+    // Get server pin
+    printf("Enter a 3 digit pin\n> ");
     for (int i = 0; i < 3; i++) {
-        scanf(" %c", &pin[i]); // Added space to consume any whitespace/newline
+        scanf(" %c", &serverPin[i]);
     }
+    printf("Server PIN set to: %c %c %c\n", serverPin[0], serverPin[1], serverPin[2]);
 
-    // Communication loop
+    // Receive client pin
+    n = recv(client_sock, buffer, 3, 0);
+    if (n < 0) die_with_error("Error: recv() Failed (receiving pin).");
+
+    for (int i = 0; i < 3; i++) {
+        clientPin[i] = buffer[i];
+    }
+ 
+    printf("Client PIN received.\n");
+    
+    // Send server pin to server
+    n = send(server_sock, serverPin, 3, 0);
+    if (n < 0) die_with_error("Error: send() Failed (sending pin).");
+
+    
+    // Communicate    
     while (1) {
-        // Send message to client
+        // Server sends the first message
         printf("< ");
         bzero(buffer, 256);
         fgets(buffer, 255, stdin);
+        
         n = send(client_sock, buffer, strlen(buffer), 0);
         if (n < 0) die_with_error("Error: send() Failed.");
-
-        // Exit check
+        
+        // End communication if server exited
         if (strncmp(buffer, "exit", 4) == 0) break;
-
-        // Receive guess from client
+        
+        // Receive from the client
         bzero(buffer, 256);
         n = recv(client_sock, buffer, 255, 0);
+        
         if (n < 0) die_with_error("Error: recv() Failed.");
+        printf("[client] > %s", buffer);
 
-        check(pin, buffer, client_sock);
-
+        // End communication if client exited
         if (strncmp(buffer, "exit", 4) == 0) break;
     }
-
-    // Clean up
+    
     printf("Closing connection ...\n");
     close(client_sock);
     close(server_sock);
+    
     return 0; 
 }
+
